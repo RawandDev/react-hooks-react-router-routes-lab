afa5678d3265594c95cc423d654e7d9627b033f5
RawandDev <rawand.200091384037@gmail.com>
2021-08-03 02:26:18 +0300

    done!

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>
+            Title: {movie.title} | Time: {movie.time}
+          </h3>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,39 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const styles = {
+    color: "black",
+    fontSize: "1.5em",
+  };
+
+  const defaultStyle = {
+    padding: "0.6rem 1rem",
+    borderBottom: "3px solid #000000b1",
+    display: "flex",
+    justifyContent: "space-evenly",
+  };
+
+  const textDecoration = {
+    textDecoration: "none",
+    color: "black",
+  };
+
+  return (
+    <div className="navbar" style={defaultStyle}>
+      <NavLink to="/" activeStyle={styles} exact style={textDecoration}>
+        Home
+      </NavLink>
+      <NavLink to="/directors" activeStyle={styles} style={textDecoration}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" activeStyle={styles} style={textDecoration}>
+        Actors
+      </NavLink>
+      <NavLink to="/movies" activeStyle={styles} style={textDecoration}>
+        Movies
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
